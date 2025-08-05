8187dbf0140d0985ff10be0109c9f44af23f2680
Kelly Malerou <kmalerou@gmail.com>
2025-08-05 17:46:05 +0300

    split to smaller methods + improvements

--- smelly-cat/src/app/components/contact/contact-us-form/contact-us-form.component.ts
+++ smelly-cat/src/app/components/contact/contact-us-form/contact-us-form.component.ts
@@ -1,23 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {
+  FormBuilder,
   FormControl,
   FormGroup,
+  FormGroupDirective,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 import { hasAcceptedValidator } from '../../../validators';
-import { EmailService } from '../../../services';
+import { EmailService, SnackbarService } from '../../../services';
 import { catchError, EMPTY, Observable, switchMap, take } from 'rxjs';
 import { NotificationEmailParams, ReplyEmailParams } from '../../../models';
 
 const SUCCESSFUL_EMAIL_SENT_MSG = 'Message successfully sent!';
+const FAILED_EMAIL_SENT_MSG = 'Failed to send email. Please try again later.';
 
 @Component({
   selector: 'app-contact-us-form',
@@ -34,29 +36,59 @@ const SUCCESSFUL_EMAIL_SENT_MSG = 'Message successfully sent!';
   styleUrl: './contact-us-form.component.scss',
 })
 export class ContactUsFormComponent {
+  @ViewChild('formDirective') private formDirective!: FormGroupDirective;
   public isSubmitting = false;
-  private _emailService = inject(EmailService);
-  private _snackBar = inject(MatSnackBar);
+  public contactForm!: FormGroup;
 
-  contactForm = new FormGroup({
-    fullName: new FormControl('', [
-      Validators.required,
-      Validators.pattern('[a-zA-Z ]*'),
-    ]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    city: new FormControl('', Validators.required),
-    postalCode: new FormControl('', Validators.required),
-    address: new FormControl('', Validators.required),
-    message: new FormControl(''),
-    hasAcceptedTermsAndConditions: new FormControl(false, [
-      hasAcceptedValidator(),
-    ]),
-  });
+  private _fb = inject(FormBuilder);
+  private _emailService = inject(EmailService);
+  private _snackbarService = inject(SnackbarService);
+
+  private readonly _initialFormState = {
+    fullName: '',
+    email: '',
+    city: '',
+    postalCode: '',
+    address: '',
+    message: '',
+    hasAcceptedTermsAndConditions: false,
+  };
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
 
   onSubmit() {
     this.isSubmitting = true;
     this.contactForm.disable();
     this.sendNotificationAndReplyEmails();
+  }
+
+  private initializeForm(): void {
+    this.contactForm = this._fb.group({
+      fullName: new FormControl(this._initialFormState.fullName, [
+        Validators.required,
+        Validators.pattern('[a-zA-Z ]*'),
+      ]),
+      email: new FormControl(this._initialFormState.email, [
+        Validators.required,
+        Validators.email,
+      ]),
+      city: new FormControl(this._initialFormState.city, Validators.required),
+      postalCode: new FormControl(
+        this._initialFormState.postalCode,
+        Validators.required,
+      ),
+      address: new FormControl(
+        this._initialFormState.address,
+        Validators.required,
+      ),
+      message: new FormControl(this._initialFormState.message),
+      hasAcceptedTermsAndConditions: new FormControl(
+        this._initialFormState.hasAcceptedTermsAndConditions,
+        [hasAcceptedValidator()],
+      ),
+    });
   }
 
   private createAutoReplyEmailParams(): ReplyEmailParams {
@@ -92,6 +124,19 @@ export class ContactUsFormComponent {
     };
   }
 
+  private handleSendNotificationEmailSuccess(): void {
+    this.isSubmitting = false;
+    this.formDirective.resetForm();
+    this.contactForm.reset({ ...this._initialFormState });
+    this.contactForm.enable();
+    this._snackbarService.openDefaultSnackbar(SUCCESSFUL_EMAIL_SENT_MSG);
+  }
+
+  private handleSendNotificationEmailFailure(): void {
+    this.contactForm.enable();
+    this._snackbarService.openDefaultSnackbar(FAILED_EMAIL_SENT_MSG);
+  }
+
   private sendNotificationAndReplyEmails(): void {
     const params = this.createNotificationEmailParams();
     this._emailService
@@ -100,15 +145,13 @@ export class ContactUsFormComponent {
         take(1),
         catchError((err) => {
           console.warn('Notification email failed:', err);
+          this.handleSendNotificationEmailFailure();
           return EMPTY;
         }),
         switchMap(() => this.sendAutoReplyEmail()),
       )
       .subscribe(() => {
-        this.isSubmitting = false;
-        this.contactForm.enable();
-        this.contactForm.reset();
-        this._snackBar.open(SUCCESSFUL_EMAIL_SENT_MSG, 'Close');
+        this.handleSendNotificationEmailSuccess();
       });
   }
 }
