7c721205e3a4e34eb249ee9678719a5ce60d2ea8
Kelly Malerou <kmalerou@gmail.com>
2025-08-07 02:14:25 +0300

    responsiveness

--- smelly-cat/src/app/app.component.ts
+++ smelly-cat/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    NavigationBarComponent,
+    MatSidenavModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  @ViewChild('drawer') private drawer!: MatSidenav;
+
+  handleOpenBurgerMenu(): void {
+    this.drawer.toggle();
+  }
+}
--- smelly-cat/src/app/components/header/header.component.ts
+++ smelly-cat/src/app/components/header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NavigationBarComponent } from '../navigation-bar/navigation-bar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [NavigationBarComponent],
+  imports: [NavigationBarComponent, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() toggleBurgerMenu = new EventEmitter<void>();
+
+  public openBurgerMenu(): void {
+    this.toggleBurgerMenu.emit();
+  }
+}
